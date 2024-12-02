15b1bc80cdfd3a82aa2ff750f74557dd987ae454
L'etranger <92956390+Letranger18@users.noreply.github.com>
2024-12-02 19:06:47 +0800

    Update JBasket.sol

--- JBasket.sol
+++ JBasket.sol
@@ -9,17 +9,24 @@ contract JBasket is ERC20, Ownable {
     string private constant _name = "Essential Gold Goods";
     string private constant _symbol = "EGG";
     uint8 private constant _decimals = 18;
-    uint256 private constant _totalSupply = 100 * 10**uint256(_decimals);
+    uint256 private constant _initialSupply = 100;
 
     constructor() ERC20(_name, _symbol) {
-        _mint(msg.sender, _totalSupply);
+        _mint(msg.sender, _initialSupply);
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
+        require(to != msg.sender, "You cannot mint tokens to your own account!");
         _mint(to, amount);
     }
 
     function burn(uint256 amount) public {
+        require(amount > 0, "You cannot burn an amount of 0 or less!");
         _burn(msg.sender, amount);
+    }
+
+    function transfer(address to, uint256 amount) public override returns (bool) {
+        require(msg.sender != to, "You cannot transfer tokens to your own account!");
+        return super.transfer(to, amount);
     }
 }
